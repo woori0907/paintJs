7109a8b14d75f15435ee2f2a3d6a112c59d73de7
Woori Kang <rose5307@naver.com>
2021-05-02 23:58:55 +0900

    마우스 이동 감지

---
+++ app.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event){
+    // clientX, Y : 윈도우 전체 안에서의 좌표
+    // canvas 안에서와 윈도우에서의 좌표값이 다르기 때문에 
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+
+}
