dd9c0aa232d8c71375f16e4253a3d86be7375582
Woori Kang <rose5307@naver.com>
2021-05-08 02:42:30 +0000

    선 그리기 기능 추가

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = true;
 function onMouseMove(event){
     // clientX, Y : 윈도우 전체 안에서의 좌표
-    // canvas 안에서와 윈도우에서의 좌표값이 다르기 때문에 
+    // canvas 안에서와 윈도우에서의 좌표값이 다르기 때문에 offsetX,Y 사용하해야함
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
+    if(!painting){
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+    }else{
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+  
 }
+
+function startPainting(){
+  
+}
+
+function stopPainting(){
+  painting = false;
+  
+}
+
+function onMouseDown(event){
+  painting = true;
+}
+
+function onMouseUp(event){
+  stopPainting();
+  
+}
+
+// canvas : context를 갖는 html요소.
+// context는 요소 안에서 픽셀에 접근할 수 있는 방법.
+// canvas에는 2d뿐만 아니라 많은 context가 있음.
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup"), onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
