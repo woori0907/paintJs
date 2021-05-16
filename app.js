f91973efc1c0847fd503b3845506fbd1c921ecc8
Woori Kang <rose5307@naver.com>
2021-05-16 10:49:40 +0900

    브러시 사이즈 조절 기능 추가

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 
 canvas.width = 700;
@@ -8,7 +10,9 @@ canvas.height = 700;
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
 
 function onMouseMove(event){
     // clientX, Y : 윈도우 전체 안에서의 좌표
@@ -17,7 +21,7 @@ function onMouseMove(event){
     const y = event.offsetY; 
     
     if(!painting){
-      ctx.beginPath(); 
+      ctx.beginPath();
       ctx.moveTo(x, y);
     }else{
       ctx.lineTo(x, y);
@@ -48,6 +52,22 @@ function onMouseUp(event){
   
 }
 
+function handRangeChange(event){
+  const size = event.target.value;
+  ctx.lineWidth=size;
+}
+
+function handleModeClick(){
+  if(filling === true){
+    filling = false;
+    mode.innerText = "Fill";
+  }
+  else{
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
 // canvas : context를 갖는 html요소.
 // context는 요소 안에서 픽셀에 접근할 수 있는 방법.
 // canvas에는 2d뿐만 아니라 많은 context가 있음.
@@ -62,3 +82,11 @@ if(canvas){
 Array.from(colors).forEach(color =>
     color.addEventListener("click", handleColorClick)
   );
+
+if(range){
+  range.addEventListener("input", handRangeChange);
+}
+
+if(mode){
+  mode.addEventListener("click", handleModeClick);
+}
